3bd559129f3ba43b0ecdaa7dc6d3d85372ccb08f
malena <mariahelenatavares160708@gmail.com>
2025-02-28 14:12:31 -0300

    arquivos bat

---
+++ tree.bat
@@ -0,0 +1,10 @@
+mkdir itape\setores
+cd itape
+cd setores
+mkdir compras contabilidade financeiro fiscal rh ti vendas
+cd . .
+cd . .
+mkdir tatui\setores
+cd tatui
+cd setores
+mkdir compras contabilidade financeiro fiscal rh ti vendas
